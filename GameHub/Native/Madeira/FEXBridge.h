968ec9c9dd279a84447b0a2cb83f4d9f641566c7
SIKEØ <157708940+ITZproVenom@users.noreply.github.com>
2026-09-15 02:02:09 +0530

    Native: FEXBridge.h match Madeira 97e2ce26 API

--- GameHub/Native/Madeira/FEXBridge.h
+++ GameHub/Native/Madeira/FEXBridge.h
@@ -11,6 +11,7 @@ extern "C" {
 bool fex_initialize(void);
 void fex_shutdown(void);
 int64_t fex_test_execute(void);
+
 typedef void (*fex_log_callback_t)(const char *message);
 void fex_set_log_callback(fex_log_callback_t callback);
 int64_t fex_get_jit_write_offset(void);
