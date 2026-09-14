6e37962d7cc2109e4e7ba698811a89118f5dac09
SIKEØ <157708940+ITZproVenom@users.noreply.github.com>
2026-09-15 01:48:19 +0530

    Native: PrefixExtractor.h matching Madeira API

--- GameHub/Native/Madeira/PrefixExtractor.h
+++ GameHub/Native/Madeira/PrefixExtractor.h
@@ -1,6 +1,14 @@
-#ifndef PREFIX_EXTRACTOR_H
-#define PREFIX_EXTRACTOR_H
+#pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Extract a gzip-compressed ustar tarball into dest_dir.
+ *  Returns 0 on success, -1 on failure.
+ */
 int madeira_extract_prefix_tgz(const char *tgz_path, const char *dest_dir);
 
+#ifdef __cplusplus
+}
 #endif
