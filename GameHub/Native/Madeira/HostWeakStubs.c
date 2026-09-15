bc0a382a425cc835523c79ceaedf4ac83ed2ac1f
SIKEØ <157708940+ITZproVenom@users.noreply.github.com>
2026-09-15 20:45:46 +0530

    Define ws_log_quiet so IPA archive links without libwineserver.a

--- GameHub/Native/Madeira/HostWeakStubs.c
+++ GameHub/Native/Madeira/HostWeakStubs.c
@@ -1,13 +1,15 @@
 /* Weak stubs for deep symbols expected from Madeira host static libs.
  * Public API (wineserver_start, wine_process_start, fex_*, madeira_extract_*)
- * is now provided by the strong implementations in WineServerBridge.m /
- * WineProcessBridge.m / FEXBridge.mm / PrefixExtractor.c.
+ * is provided by WineServerBridge.m / WineProcessBridge.m / FEXBridge.mm /
+ * PrefixExtractor.c.
  */
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
 
-/* ---- Symbols expected from host static libs (weak until force_load) ---- */
+/* Defined here (strong) so Swift MadeiraBootSequence can set ws_log_quiet
+ * even when the Madeira wineserver archive is not linked. */
+volatile int ws_log_quiet = 0;
 
 __attribute__((weak)) int wineserver_main(int argc, char *argv[]) {
     (void)argc; (void)argv;
@@ -21,7 +23,6 @@ __attribute__((weak)) void ws_log(const char *fmt, ...) { (void)fmt; }
 
 __attribute__((weak)) int foreground = 1;
 __attribute__((weak)) int debug_level = 0;
-/* g_wineserver_should_stop is defined strongly in WineServerBridge.m */
 
 __attribute__((weak)) void __wine_main(int argc, char *argv[]) {
     (void)argc; (void)argv;
@@ -33,10 +34,9 @@ __attribute__((weak)) void wine_ui_log(const char *message) {
     if (message) fprintf(stderr, "%s\n", message);
 }
 
-/* Optional FEX-related counters / logs that FEXCore may reference */
-__attribute__((weak)) uint64_t IosCbEntryLog[8] = {};
-__attribute__((weak)) uint64_t IosFfsBypassLog[8] = {};
-__attribute__((weak)) volatile uint64_t g_madeira_thr_count[16] = {};
-__attribute__((weak)) volatile uint64_t g_madeira_hot_count[64] = {};
-__attribute__((weak)) volatile uint64_t g_madeira_syscall_count[32] = {};
+__attribute__((weak)) uint64_t IosCbEntryLog[8] = {0};
+__attribute__((weak)) uint64_t IosFfsBypassLog[8] = {0};
+__attribute__((weak)) volatile uint64_t g_madeira_thr_count[16] = {0};
+__attribute__((weak)) volatile uint64_t g_madeira_hot_count[64] = {0};
+__attribute__((weak)) volatile uint64_t g_madeira_syscall_count[32] = {0};
 __attribute__((weak)) volatile int g_madeira_ios_host = 1;
